fdf1f5d0ab0cdd8440729ea81a564d2f2f567706
remisany <remi.d.sany@gmail.com>
2021-09-22 15:23:43 +0200

    Création 2ème exemple

--- src/stories/datepicker.stories.js
+++ src/stories/datepicker.stories.js
@@ -17,3 +17,45 @@ stories.add("Ex1", () => {
         </div>
     )
 });
+
+stories.add("Ex2", () => {
+  const date = new Date()
+
+  const customDatePicker = {
+    borderRadius: "5px",
+    border: "none",
+    backgroundColor: "rgba(240, 67, 41, .3)",
+    fontFamily: "Roboto",
+    fontSize: "15px",
+    padding: "0",
+    rowGap: "5px"
+  }
+
+  const customSelectedDay = {
+    backgroundColor: "#594B94"
+  }
+
+  const customHeader = {
+    borderRadius: "5px 5px 0 0",
+    padding: "4px 0",
+    backgroundColor: "rgba(240, 67, 41, .5)"
+  }
+
+  const customContainerNumberDay = {
+    padding: "4px 0"
+  }
+
+  return (
+    <div>
+    <h1>React Datepicker</h1>
+    <h2>Customized</h2>
+    <Datepicker
+      date = {date}
+      styleDatePicker = {customDatePicker}
+      styleSelectedDay = {customSelectedDay}
+      styleHeader = {customHeader}
+      styleContainerNumberDay = {customContainerNumberDay}
+    />
+    </div>
+  )
+});
