89c2dd2edcb90822d83ef1940899788a7696d94b
remisany <remi.d.sany@gmail.com>
2021-10-04 14:36:36 +0200

    MAJ bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,8 @@
 import babel from "@rollup/plugin-babel"
 import image from '@rollup/plugin-image';
+import noderesolve from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
     input: 'src/index.jsx',
@@ -8,6 +11,9 @@ export default {
       format: 'cjs'
     },
     plugins: [
+      commonjs(),
+      noderesolve(),
+      terser(),
       babel({
         exclude: "node_module/**",
         presets: ['@babel/preset-react']
